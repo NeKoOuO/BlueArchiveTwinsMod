162290a8bdd0ac2b6164d52c05b1a8debcfabace
马老师 <iammaming@126.com>
2024-04-02 21:39:18 +0800

    修改‘经验大礼包效果’

--- blue_archive_mod/src/main/java/baModDeveloper/cards/BATwinsExperienceGiftPackage.java
+++ blue_archive_mod/src/main/java/baModDeveloper/cards/BATwinsExperienceGiftPackage.java
@@ -17,7 +17,7 @@ public class BATwinsExperienceGiftPackage extends BATwinsModCustomCard {
     private static final CardStrings CARD_STRINGS = CardCrawlGame.languagePack.getCardStrings(ID);
     private static final String NAME = CARD_STRINGS.NAME;
     private static final String IMG_PATH = ModHelper.makeImgPath("cards", "ExperienceGiftPackage");
-    private static final int COST = 5;
+    private static final int COST = 8;
     private static final String DESCRIPTION = CARD_STRINGS.DESCRIPTION;
     private static final CardType TYPE = CardType.SKILL;
     private static final CardColor COLOR = BATwinsCharacter.Enums.BATWINS_MIDORI_CARD;
@@ -46,7 +46,7 @@ public class BATwinsExperienceGiftPackage extends BATwinsModCustomCard {
     public void applyPowers() {
         super.applyPowers();
         if (AbstractDungeon.player.hand.contains(this) && AbstractDungeon.player.hasPower(BATwinsExperiencePower.POWER_ID)) {
-            if (this.costForTurn == AbstractDungeon.player.getPower(BATwinsExperiencePower.POWER_ID).amount) {
+            if (this.costForTurn <= AbstractDungeon.player.getPower(BATwinsExperiencePower.POWER_ID).amount) {
                 addToBot(new BATwinsPlayHandCardAction(this, null));
             }
         }
@@ -62,7 +62,7 @@ public class BATwinsExperienceGiftPackage extends BATwinsModCustomCard {
     public void upgrade() {
         if (!this.upgraded) {
             this.upgradeName();
-            this.upgradeBaseCost(6);
+//            this.upgradeBaseCost(8);
             this.upgradeMagicNumber(1);
         }
     }
