c482f245da940c4a8f8b07c2af93a10bed20ff2c
马老师 <iammaming@126.com>
2024-05-04 20:46:16 +0800

    新遗物：复古游戏机

--- blue_archive_mod/src/main/java/baModDeveloper/relic/BATwinsRetroGame.java
+++ blue_archive_mod/src/main/java/baModDeveloper/relic/BATwinsRetroGame.java
@@ -1,26 +1,45 @@
 package baModDeveloper.relic;
 
+import baModDeveloper.action.BATwinsExchangeAllAction;
+import baModDeveloper.character.BATwinsCharacter;
 import baModDeveloper.helpers.ModHelper;
 import baModDeveloper.helpers.TextureLoader;
 import basemod.abstracts.CustomRelic;
 import com.badlogic.gdx.graphics.Texture;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 
 public class BATwinsRetroGame extends CustomRelic {
     public static final String ID= ModHelper.makePath("RetroGame");
     private static final Texture texture= TextureLoader.getTexture(ModHelper.makeImgPath("relic","RetroGame"));
     private static final Texture outline=TextureLoader.getTexture(ModHelper.makeImgPath("relic","RetroGame_p"));
-    private static final RelicTier type=RelicTier.COMMON;
+    private static final RelicTier type=RelicTier.BOSS;
+
+    private final AbstractCard.CardColor[] colors=new AbstractCard.CardColor[2];
     public BATwinsRetroGame() {
         super(ID, texture,outline,type, LandingSound.MAGICAL);
+        colors[0]= BATwinsCharacter.Enums.BATWINS_MOMOI_CARD;
+        colors[1]=BATwinsCharacter.Enums.BATWINS_MIDORI_CARD;
+    }
+
+    @Override
+    public String getUpdatedDescription() {
+        return DESCRIPTIONS[0];
     }
 
     @Override
     public void atTurnStartPostDraw() {
-        super.atTurnStartPostDraw();
+        addToBot(new BATwinsExchangeAllAction(colors[AbstractDungeon.cardRandomRng.random(0,1)]));
     }
 
     @Override
-    public void atTurnStart() {
-        super.atTurnStart();
+    public void onEquip() {
+        AbstractDungeon.player.masterHandSize+=3;
+    }
+
+    @Override
+    public void onUnequip() {
+        AbstractDungeon.player.masterHandSize-=3;
     }
 }
