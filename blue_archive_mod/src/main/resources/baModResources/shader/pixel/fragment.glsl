ac7c0bc2dcc485e8008fcd0a63a4b95ada0636e5
马老师 <iammaming@126.com>
2024-06-05 19:36:33 +0800

    删去不必要的shader代码

--- blue_archive_mod/src/main/resources/baModResources/shader/pixel/fragment.glsl
+++ blue_archive_mod/src/main/resources/baModResources/shader/pixel/fragment.glsl
@@ -6,7 +6,6 @@
 #endif
 
 #define res (iResolution.xy/pw)
-varying LOWP vec4 v_color;
 varying vec2 v_texCoords;
 uniform float pw;
 
@@ -16,9 +15,5 @@ uniform vec2 iResolution;
 void main() {
         vec2 uv=gl_FragCoord.xy/iResolution.xy;
         vec2 pos=floor(uv*res)/res;
-        if(max(abs(pos.x-0.5),abs(pos.y-0.5))>0.5){
-            gl_FragColor=vec4(0.0,0.0,0.0,1.0);
-        }else{
-            gl_FragColor=texture2D(u_texture,pos,-16.0);
-        }
+        gl_FragColor=texture2D(u_texture,pos,-16.0);
 }
