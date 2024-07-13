54c3c789d2666ce5b204acba5ef84d4a931c0f12
马老师 <iammaming@126.com>
2024-07-14 00:01:09 +0800

    新增遗物：总力战

---
+++ blue_archive_mod/src/main/java/baModDeveloper/patch/CombatRewardScreenPatch.java
@@ -0,0 +1,15 @@
+//package baModDeveloper.patch;
+//
+//import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
+//import com.evacipated.cardcrawl.modthespire.lib.SpirePostfixPatch;
+//import com.megacrit.cardcrawl.screens.CombatRewardScreen;
+//
+//public class CombatRewardScreenPatch {
+//    @SpirePatch(clz = CombatRewardScreen.class,method = "setupItemReward")
+//    public static class setupItemRewardPatch{
+//        @SpirePostfixPatch
+//        public static void postFixPatch(CombatRewardScreen __instance){
+//
+//        }
+//    }
+//}
--- blue_archive_mod/src/main/java/baModDeveloper/relic/BATwinsRankIcon.java
+++ blue_archive_mod/src/main/java/baModDeveloper/relic/BATwinsRankIcon.java
@@ -6,12 +6,19 @@ import basemod.abstracts.CustomRelic;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.files.FileHandle;
 import com.badlogic.gdx.graphics.Texture;
+import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.core.Settings;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.localization.RelicStrings;
 import com.megacrit.cardcrawl.relics.AbstractRelic;
+import com.megacrit.cardcrawl.rewards.RewardItem;
 import com.megacrit.cardcrawl.screens.stats.RunData;
+import com.megacrit.cardcrawl.vfx.UpgradeShineEffect;
+import com.megacrit.cardcrawl.vfx.cardManip.ShowCardBrieflyEffect;
 
 import java.lang.reflect.Field;
+import java.util.ArrayList;
 
 public class BATwinsRankIcon extends CustomRelic {
     public static final String ID= ModHelper.makePath("RankIcon");
@@ -47,6 +54,9 @@ public class BATwinsRankIcon extends CustomRelic {
             field.set(this,relicStrings[this.rank-1].NAME);
         } catch (NoSuchFieldException | IllegalAccessException e) {
             throw new RuntimeException(e);
+        }
+        if(this.rank<=2){
+            this.counter=0;
         }
     }
 
@@ -88,16 +98,60 @@ public class BATwinsRankIcon extends CustomRelic {
         if(score==-1){
             return 1;
         }
-        if(score<500){
+        if(score<1000){
             return 4;
-        }else if(score<1000){
+        }else if(score<2000){
             return 3;
-        }else if(score<1500){
+        }else if(score<3000){
             return 2;
         }else{
             return 1;
         }
     }
 
+    @Override
+    public void onVictory() {
+        this.flash();
+        switch (this.rank){
+            case 1:
+            case 2:
+                AbstractDungeon.player.gainGold(15);
+                this.counter++;
+                break;
+            case 3:
+                AbstractDungeon.player.gainGold(15);
+                break;
+            case 4:
+                AbstractDungeon.player.gainGold(10);
+                break;
+            default:
+                break;
+        }
 
+        if((this.counter==4&&this.rank==1)||(this.counter==5&&this.rank==2)){
+            AbstractCard card=getCard();
+            if(card!=null){
+                card.upgrade();
+                AbstractDungeon.player.bottledCardUpgradeCheck(card);
+                AbstractDungeon.effectsQueue.add(new UpgradeShineEffect(Settings.WIDTH / 2.0F, Settings.HEIGHT / 2.0F));
+                AbstractDungeon.topLevelEffectsQueue.add(new ShowCardBrieflyEffect(card.makeStatEquivalentCopy()));
+            }
+            this.counter=0;
+        }
+    }
+
+    private AbstractCard getCard(){
+        ArrayList<AbstractCard> cards=new ArrayList<>();
+        AbstractCard res = null;
+        for(AbstractCard card:AbstractDungeon.player.masterDeck.group){
+            if(card.canUpgrade()){
+                cards.add(card);
+            }
+        }
+        if(!cards.isEmpty()){
+            res = cards.get(AbstractDungeon.miscRng.random(0, cards.size() - 1));
+
+        }
+        return res;
+    }
 }
