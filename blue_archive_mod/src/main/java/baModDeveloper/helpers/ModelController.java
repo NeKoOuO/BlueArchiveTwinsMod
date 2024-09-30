e65ec1cdf5d52bc0331271200792ca3589e2950b
马老师 <iammaming@126.com>
2024-09-30 19:14:44 +0800

    增加抗锯齿

--- blue_archive_mod/src/main/java/baModDeveloper/helpers/ModelController.java
+++ blue_archive_mod/src/main/java/baModDeveloper/helpers/ModelController.java
@@ -14,7 +14,7 @@ import java.util.function.Consumer;
 
 public class ModelController {
     private static final float SCALE = 4.5F * Settings.scale*Character3DHelper.ExpandScale;
-    private static final float MOVESCALE = Settings.scale * 4;
+    private static final float MOVESCALE = Settings.xScale * 4;
     private G3dModelLoader loader;
     private Model model;
     private ModelInstance instance;
